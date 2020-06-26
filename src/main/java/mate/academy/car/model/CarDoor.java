17c2984fd146b2808709072e180d49eb42f08341
olehedza <olehedza@gmail.com>
2020-06-26 23:29:17 +0300

    add models

---
+++ src/main/java/mate/academy/car/model/CarDoor.java
@@ -0,0 +1,25 @@
+package mate.academy.car.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "car_door")
+@Data
+@NoArgsConstructor
+public class CarDoor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Mode doorMode;
+    @Enumerated(EnumType.STRING)
+    private Mode windowMode;
+}
---
+++ src/main/java/mate/academy/car/model/Mode.java
@@ -0,0 +1,6 @@
+package mate.academy.car.model;
+
+public enum Mode {
+    OPEN,
+    CLOSED
+}
